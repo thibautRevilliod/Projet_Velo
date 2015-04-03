2584dea6e2a1855e1dacc2724be7cb31fab4947b
Thibaut Revilliod <thibaut.revilliod@gmail.com>
2015-04-03 14:38:23 +0200

    Add basic classes

---
+++ src/metier/Operateur.java
@@ -0,0 +1,10 @@
+package metier;
+
+public class Operateur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/metier/Velo.java
@@ -0,0 +1,10 @@
+package metier;
+
+public class Velo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
