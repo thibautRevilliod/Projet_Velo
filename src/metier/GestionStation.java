d56d76d59cd0f741d2301fbc31af219552615be7
Thibaut Revilliod <thibaut.revilliod@gmail.com>
2015-04-09 18:38:42 +0200

    gestion exception RMI dans GestionStation

--- src/metier/GestionStation.java
+++ src/metier/GestionStation.java
@@ -1,30 +1,30 @@
 package metier;
 
-public interface GestionStation {
-	public void ajouterStation(Station station);
+public interface GestionStation extends java.rmi.Remote {
+	public void ajouterStation (Station station) throws java.rmi.RemoteException;
 	
-	public void supprimerStation(Station station);
+	public void supprimerStation(Station station) throws java.rmi.RemoteException;
 	
 	public int creerOperateur(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, 
-			String padressepostale);
+			String padressepostale) throws java.rmi.RemoteException;
 
 	public int creerAdministrateur(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, 
-			String padressepostale);
+			String padressepostale) throws java.rmi.RemoteException;
 	
 	public int creerClient(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, 
-			String padressepostale);
+			String padressepostale) throws java.rmi.RemoteException;
 	
-	public void ajouterVeloStation(Velo velo, Station station);
+	public void ajouterVeloStation(Velo velo, Station station) throws java.rmi.RemoteException;
 	
-	public void supprimerVeloStation(Velo velo, Station station);
+	public void supprimerVeloStation(Velo velo, Station station) throws java.rmi.RemoteException;
 	
-	public void transfererVelo(Velo velo, Station stationOrigine, Station stationDestination);
+	public void transfererVelo(Velo velo, Station stationOrigine, Station stationDestination) throws java.rmi.RemoteException;
 	
-	public void ajouterUtilisateur (Utilisateur utilisateur);
+	public void ajouterUtilisateur (Utilisateur utilisateur) throws java.rmi.RemoteException;
 	
-	public void supprimerUtilisateur (Utilisateur utilisateur);
+	public void supprimerUtilisateur (Utilisateur utilisateur) throws java.rmi.RemoteException;
 	
-	public void emprunterVeloClient(Client client, Velo velo, Station station);
+	public void emprunterVeloClient(Client client, Velo velo, Station station) throws java.rmi.RemoteException;
 	
-	public void ramenerVeloClient(Client client, Velo velo, Station station);
+	public void ramenerVeloClient(Client client, Velo velo, Station station) throws java.rmi.RemoteException;
 }
--- src/metier/GestionStationImpl.java
+++ src/metier/GestionStationImpl.java
@@ -1,95 +1,97 @@
 package metier;
 
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.util.HashMap;
 
 public class GestionStationImpl implements GestionStation {
 	
 	@Override
-	public void ajouterStation(Station station)
+	public synchronized void ajouterStation(Station station) throws RemoteException
 	{
 		Station.lesStations.put(station.getIdStation(), station);
 	}
 	
 	@Override
-	public void supprimerStation(Station station)
+	public synchronized void supprimerStation(Station station) throws RemoteException
 	{
 		Station.lesStations.remove(station.getIdStation());
 	}
 	
 	@Override
-	public int creerOperateur(String pnom, String pprenom, String pmotdepasse,
-			String ptelephone, String padressemail, String padressepostale) {
-		Operateur operateur = new Operateur( pnom,  pprenom, pmotdepasse, ptelephone,  padressemail, 
-				 padressepostale);
+	public synchronized int creerOperateur(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, String padressepostale) throws RemoteException 
+	{
+		Operateur operateur = new Operateur( pnom,  pprenom, pmotdepasse, ptelephone,  padressemail, padressepostale);
 		return operateur.getIdUtilisateur();
-		
 	}
 	
 	@Override
-	public int creerAdministrateur(String pnom, String pprenom, String pmotdepasse,
-			String ptelephone, String padressemail, String padressepostale) {
-		Administrateur adminstrateur = new Administrateur( pnom,  pprenom, pmotdepasse, ptelephone,  padressemail, 
-				 padressepostale);
+	public synchronized int creerAdministrateur(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, String padressepostale) throws RemoteException 
+	{
+		Administrateur adminstrateur = new Administrateur( pnom,  pprenom, pmotdepasse, ptelephone,  padressemail, padressepostale);
 		return adminstrateur.getIdUtilisateur();
-		
 	}
 	
 	@Override
-	public int creerClient(String pnom, String pprenom, String pmotdepasse,
-			String ptelephone, String padressemail, String padressepostale) {
-		Client client = new Client( pnom, pprenom, pmotdepasse, ptelephone, padressemail, 
-				 padressepostale);
+	public synchronized int creerClient(String pnom, String pprenom, String pmotdepasse, String ptelephone, String padressemail, String padressepostale) throws RemoteException 
+	{
+		Client client = new Client( pnom, pprenom, pmotdepasse, ptelephone, padressemail, padressepostale);
 		return client.getIdUtilisateur();
-		
 	}
 	
 	@Override
-	public void ajouterVeloStation(Velo velo, Station station)
+	public synchronized void ajouterVeloStation(Velo velo, Station station) throws RemoteException
 	{
 		velo.setStation(station);
 		station.ajouterVelo(velo);
 	}
 	
 	@Override
-	public void supprimerVeloStation(Velo velo, Station station)
+	public synchronized void supprimerVeloStation(Velo velo, Station station) throws RemoteException
 	{
 		velo.setStation(null);
 		station.supprimerVelo(velo);
 	}
 	
 	@Override
-	public void transfererVelo(Velo velo, Station stationOrigine, Station stationDestination)
+	public synchronized void transfererVelo(Velo velo, Station stationOrigine, Station stationDestination) throws RemoteException
 	{
 		supprimerVeloStation(velo, stationOrigine);
 		ajouterVeloStation(velo, stationDestination);
 	}
 	
 	@Override
-	public void ajouterUtilisateur (Utilisateur utilisateur)
+	public synchronized void ajouterUtilisateur (Utilisateur utilisateur) throws RemoteException
 	{
 		utilisateur.lesUtilisateurs.put(utilisateur.getIdUtilisateur(), utilisateur);
 	}
 	
 	@Override
-	public void supprimerUtilisateur (Utilisateur utilisateur)
+	public synchronized void supprimerUtilisateur (Utilisateur utilisateur) throws RemoteException
 	{
 		utilisateur.lesUtilisateurs.remove(utilisateur.getIdUtilisateur());
 	}	
 	
 	@Override
-	public void emprunterVeloClient(Client client, Velo velo, Station station)
+	public synchronized void emprunterVeloClient(Client client, Velo velo, Station station) throws RemoteException
 	{
 		client.setVelo(velo);
 		station.lesVelos.remove(velo);
 	}
 	
 	@Override
-	public void ramenerVeloClient(Client client, Velo velo, Station station)
+	public synchronized void ramenerVeloClient(Client client, Velo velo, Station station) throws RemoteException
 	{
 		client.setVelo(null);
 		station.lesVelos.add(velo);
 	}
 
-	
+	public synchronized static void main(String[] args) throws Exception {
+		LocateRegistry.createRegistry(1099);
+		Naming.rebind("MaGestionStation",new GestionStationImpl());
+		System. out.println("MaGestionStation est enregistrée");
+	}
 
 }
