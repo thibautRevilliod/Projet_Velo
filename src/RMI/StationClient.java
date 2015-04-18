d9e4dcf900e38fb45e9297e546d16488b0227724
Landry <aaa@gfhgf.jh>
2015-04-18 14:52:38 +0200

    Commit Menu Utilisateur du vendredi 10 avril 2015

--- src/RMI/StationClient.java
+++ src/RMI/StationClient.java
@@ -1,5 +1,8 @@
 package RMI;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.Scanner;
@@ -8,23 +11,20 @@ import metier.GestionStation;
 
 public class StationClient {
 	public static GestionStation proxyGS;
-	public static int valeurChoix;
-	public static Scanner sc = new Scanner(System.in);
+	public static String valeurChoix;
+	//public static Scanner sc = new Scanner(System.in);
+	public static BufferedReader entree = new BufferedReader(new InputStreamReader(System.in)); 
 	
-	public static void menuPrincipal(){
+	public static void menuPrincipal() throws IOException{
 		System.out.println("------------ Bienvenue ------------");
 		System.out.println("--1) Créer compte");
 		System.out.println("--2) S'identifier");
 		System.out.println("------------ Fin ------------");
-		valeurChoix = sc.nextInt();
+		valeurChoix = entree.readLine();
 	}
 	
-	public static void menuCreerCompte() throws RemoteException{
-		String nom;
-		String prenom;
-		String telephone;
-		String adresseMail;
-		String adressePostale;
+	public static void menuCreerCompte() throws IOException{
+		String tab[];
 		String mdp;
 		int identifiantUtilisateur = 0;
 		
@@ -33,52 +33,64 @@ public class StationClient {
 		System.out.println("--2) Operateur");
 		System.out.println("--3) Administrateur");
 		System.out.println("--4) Quitter");
-		valeurChoix = sc.nextInt();
-		
-		System.out.println("Veuillez entrer votre nom : ");
-		nom = sc.next();
-		System.out.println("Veuillez entrer votre prenom : ");
-		prenom = sc.nextLine();
-		System.out.println("Veuillez entrer votre telephone : ");
-		telephone = sc.nextLine();
-		System.out.println("Veuillez entrer votre email : ");
-		adresseMail = sc.nextLine();
-		System.out.println("Veuillez entrer votre adresse postale : ");
-		adressePostale = sc.next();
-		System.out.println("Veuillez entrer votre mot de passe : ");
-		mdp = sc.next();
-		
+		valeurChoix = entree.readLine();
+			
 		switch(valeurChoix)
 		{
-		case 1:
-			identifiantUtilisateur = proxyGS.creerClient(nom, prenom, mdp, telephone, adresseMail, adressePostale);
-			System.out.println("Utilisateur créé");
-			System.out.println("  Identifiant : " + identifiantUtilisateur);
-			System.out.println("  Mot de passe : " + mdp);		
+		case "1":
+			tab = demandeInfoCreeUtilisateur();
+			identifiantUtilisateur = proxyGS.creerUtilisateur(tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], "Client");
+			messageUtilisateurCree(identifiantUtilisateur, mdp);	
 			break;
-		case 2:
-			identifiantUtilisateur = proxyGS.creerOperateur(nom, prenom, mdp, telephone, adresseMail, adressePostale);
-			System.out.println("Utilisateur créé");
-			System.out.println("  Identifiant : " + identifiantUtilisateur);
-			System.out.println("  Mot de passe : " + mdp);		
+		case "2":
+			tab = demandeInfoCreeUtilisateur();
+			identifiantUtilisateur = proxyGS.creerUtilisateur(tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], "Operateur");
+			messageUtilisateurCree(identifiantUtilisateur, mdp);	
 			break;
-		case 3:
-			identifiantUtilisateur = proxyGS.creerAdministrateur(nom, prenom, mdp, telephone, adresseMail, adressePostale);
-			System.out.println("Utilisateur créé");
-			System.out.println("  Identifiant : " + identifiantUtilisateur);
-			System.out.println("  Mot de passe : " + mdp);		
+		case "3":
+			tab = demandeInfoCreeUtilisateur();
+			identifiantUtilisateur = proxyGS.creerUtilisateur(tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], "Administrateur");
+			messageUtilisateurCree(identifiantUtilisateur, mdp);
 			break;
+		case "4":
+			menuPrincipal();
 		}	
 	}
 	
-	public static void menuIdentification() throws RemoteException
+	public static void messageUtilisateurCree(int pidentifiantUtilisateur, String pmdp){
+		System.out.println("Utilisateur créé");
+		System.out.println("  Identifiant : " + pidentifiantUtilisateur);
+		System.out.println("  Mot de passe : " + pmdp);	
+	}
+	
+	public static String[] demandeInfoCreeUtilisateur() throws IOException{	
+		String result[] = new String[6];
+		
+		System.out.println("Veuillez entrer votre nom : ");
+		result[0] = entree.readLine();
+		System.out.println("Veuillez entrer votre prenom : ");
+		result[1] = entree.readLine();
+		System.out.println("Veuillez entrer votre telephone : ");
+		result[2] = entree.readLine();
+		System.out.println("Veuillez entrer votre email : ");
+		result[3] = entree.readLine();
+		System.out.println("Veuillez entrer votre adresse postale : ");
+		result[4] = entree.readLine();
+		System.out.println("Veuillez entrer votre mot de passe : ");
+		result[5] = entree.readLine();
+		
+		return result;
+	}
+	
+	public static void menuIdentification() throws NumberFormatException, IOException
 	{
 		int identifiant;
 		String mdp;
 		System.out.println("Veuillez entrer votre identifiant : ");
-		identifiant = sc.nextInt();
+		identifiant = Integer.valueOf(entree.readLine());
 		System.out.println("Veuillez entrer votre mot de passe : ");
-		mdp = sc.next();
+		mdp = entree.readLine();
+		// rôle ! ! ! !
 		boolean reponsOk = proxyGS.estUtilisateurIdentifie(identifiant, mdp);
 		if(reponsOk)
 		{
@@ -86,70 +98,28 @@ public class StationClient {
 		}else
 		{
 			System.out.println("Connexion refusée");
+			
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		proxyGS = (GestionStation) Naming.lookup("rmi://localhost:1099/MaGestionStation");
-		Boolean exit = false;
+//		Boolean exit = false;
 		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int nbre;
 		//menu
-		while(!exit)
-		{
+//		while(!exit)
+//		{
 			menuPrincipal();
 			
 			switch(valeurChoix)
 			{
-				case 1:
+				case "1":
 					menuCreerCompte();
-				case 2:
+				case "2":
 					menuIdentification();
+					
 			}
-			
-			System.out.println("--2) Emprunter un vélo");
-			System.out.println("--3) Ramener un vélo");
-			System.out.println("--4) Quitter");
-			
-			
-			
-			
-//			switch(val)
-//			{
-//			case 1:
-//				System.out.println("Veuillez entrer votre nom : ");
-//				nom = sc.next();
-//				System.out.println("Veuillez entrer votre prenom : ");
-//				prenom = sc.next();
-//				System.out.println("Veuillez entrer votre telephone : ");
-//				telephone = sc.next();
-//				System.out.println("Veuillez entrer votre email : ");
-//				adresseMail = sc.next();
-//				System.out.println("Veuillez entrer votre adresse postale : ");
-//				adressePostale = sc.next();
-//				proxyGS.creerUtilisateur(nom, prenom, telephone, adresseMail, ;
-//				break;
-//			case 2:
-//				System.out.println("Veuillez entrer le nom du compte : ");
-//				cpte = sc.next();
-//				System.out.println("Veuillez entrer la valeur : ");
-//				nbre = sc.nextInt();
-//				proxy.ajouter(cpte, nbre);
-//				System.out.println("Position du compte de " + cpte + " : "+proxy.position(cpte));
-//				break;
-//			case 3:
-//				System.out.println("Veuillez entrer le nom du compte : ");
-//				cpte = sc.next();
-//				System.out.println("Veuillez entrer la valeur : ");
-//				nbre = sc.nextInt();
-//				proxy.retirer(cpte, nbre);
-//				System.out.println("Position du compte de " + cpte + " : "+proxy.position(cpte));
-//				break;
-//			case 4:
-//				exit = true;
-//				break;
-//			}
-		}
+//		}
 	}
 }
--- src/metier/CarteAcces.java
+++ src/metier/CarteAcces.java
@@ -1,8 +1,5 @@
 package metier;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class CarteAcces {
 	
 	public enum Role{Administrateur, Operateur, Client};
